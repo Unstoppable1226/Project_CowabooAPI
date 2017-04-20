ddec54761571530b0c9033e713d6818326e6ae16
Unstoppable1226 <joel.10joel.10@gmail.com>
2017-04-20 15:44:48 +0200

    Insertion Activité SearchActivity

---
+++ MediKit/app/src/main/java/com/example/pixa/medikit/Presentation/SearchActivity.java
@@ -0,0 +1,23 @@
+package com.example.pixa.medikit.Presentation;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.example.pixa.medikit.R;
+
+/**
+ * Created by Joel Marques on 20.04.2017.
+ */
+
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_diseases);
+    }
+
+}
